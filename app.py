52a6f68a18912be6447a81ff0eb5fc0b826d8d2f
Endowsan <ppqr025@gmail.com>
2022-12-10 18:35:01 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,31 @@
+# 1. ライブラリをインポートする
+import requests
+from bs4 import BeautifulSoup
+import streamlit as st
+import random
+
+# 2. スクレイピングするWebサイトのURLを指定する
+url = "https://www.mcdonalds.co.jp/menu/"
+
+# 3. WebサイトからHTMLデータを取得し、適切な形式で保存する
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+
+# 4. 取得したHTMLデータを解析し、商品名と料金を抽出する
+lineup = [item.text for item in soup.find_all(class_="product-list-card-name font-bold mt-2 mb-2 mb-4:md")]
+price =  [item.text.replace("~","") for item in soup.find_all(class_="product-list-card-price-number text-2xl font-extrabold")] 
+products =[]
+
+for i in range(len(price)):
+    products.append({"name":lineup[i],"price":price[i]})
+    
+    
+
+
+
+st.sidebar.title("商品一覧")
+for product in products:
+    st.sidebar.markdown(f"- {product['name']}: {product['price']}円")
+
+
+
